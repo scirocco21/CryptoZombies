805167ff5840406d67349744bc5ea9f7a11938dd
Sebastian <sebgertz@gmail.com>
2018-11-11 14:01:33 -0500

    added mapping from zombie to owner and from owner to zombie count

--- Contract.sol
+++ Contract.sol
@@ -15,6 +15,10 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
     function _createZombie(string _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
         emit NewZombie(id, _name, _dna);
