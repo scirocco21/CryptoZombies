4d2f392cae547a7f9642b0727eb134d0ddb25e93
Sebastian <sebgertz@gmail.com>
2018-11-11 21:09:37 -0500

    mapped zombie id to owner in createZombie private functiona and incremented owners zombie count

--- Contract.sol
+++ Contract.sol
@@ -21,6 +21,8 @@ contract ZombieFactory {
 
     function _createZombie(string _name, uint _dna) private {
         uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
         emit NewZombie(id, _name, _dna);
         // and fire it here
     }
