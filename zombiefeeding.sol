fe9a669b7715f9ecf18c2c9f4903fb42512a24bb
Sebastian <sebgertz@gmail.com>
2018-11-14 11:44:52 -0500

    modified feedAndMultiple function to accept species param and modify zombie Dna in light of it

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -19,11 +19,19 @@ contract KittyInterface {
 
 contract ZombieFeeding is ZombieFactory {
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  // Modify function definition here:
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
+    // Add an if statement here
+    if (keccak256(abi.encodePacked(_species)) == keccak256(abi.encodePacked("kitty"))) {
+      newDna = newDna - newDna %100 + 99;
+    }
     _createZombie("NoName", newDna);
   }
 
@@ -31,6 +39,6 @@ contract ZombieFeeding is ZombieFactory {
     uint kittyDna;
     (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
     // And modify function call here:
-    feedAndMultiply(_zombieId, kittyDna);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
   }
 }
